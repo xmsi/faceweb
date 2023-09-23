d529945a6509d4352c6eab6a5fed4c366b2928c4
sattorovsharofiddin <cyberpancher10@gmail.com>
2023-09-23 12:37:51 +0500

    add models.py

---
+++ models.py
@@ -0,0 +1,32 @@
+import typing as t
+from datetime import datetime
+
+from sqlalchemy import String, DateTime, func, Integer, ForeignKey, Boolean
+from sqlalchemy.ext.declarative import as_declarative, declared_attr
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+class_registry: t.Dict = {}
+
+
+@as_declarative(class_registry=class_registry)
+class Base:
+    id: t.Any
+    __name__: str
+
+    # Generate __tablename__ automatically
+    @declared_attr
+    def __tablename__(cls) -> str:
+        return cls.__name__.lower()
+
+
+class Users(Base):
+    name: Mapped[str] = mapped_column(String(100))
+    payments: Mapped[list['Payment']] = relationship(back_populates='payments', lazy='selectin')
+
+
+class Payment(Base):
+    image: Mapped[str] = mapped_column(String(255))
+    created_at: Mapped[datetime] = mapped_column(DateTime, server_default=func.now())
+    user_id: Mapped[int] = mapped_column(Integer, ForeignKey('users.id', ondelete='CASCADE'), nullable=False)
+    user: Mapped[list['Users']] = relationship(back_populates='users')
+    status: Mapped[bool] = mapped_column(Boolean, default=False)
